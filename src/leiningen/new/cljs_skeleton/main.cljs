6b707ca93544095b591e81fa96988c56cd7c877a
Joc <jocphone+gh@gmail.com>
2013-07-21 23:02:31 +0100

    Revert "Switched from Dommy to Jayq"
    This reverts commit fac15661364e1efb778e26c1f41892e0cbb16d7e.

--- src/leiningen/new/cljs_skeleton/main.cljs
+++ src/leiningen/new/cljs_skeleton/main.cljs
@@ -1,6 +1,9 @@
 (ns {{name}}.main
- (:use
-  [jayq.core :only [$ css html text on]]))
+ (:use-macros
+  [dommy.macros :only [sel sel1]])
+ (:require
+   [dommy.utils :as utils]
+   [dommy.core :as dommy]))
 
 
 (js/alert "Working!")
